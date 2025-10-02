b0342768ea389a129b5d717d8de0d02042d00ebb
Isabellaariza <isabellaariza7@gmail.com>
2025-10-02 09:26:56 -0500

    Fix: Conexión MongoDB hardcodeada temporalmente

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect("mongodb+srv://isabellaariza7:1234@cluster0.ofsvfqu.mongodb.net/sgsp_db?retryWrites=true&w=majority&appName=Cluster0");
     console.log('MongoDB conectado correctamente');
   } catch (error) {
     console.error('Error conectando a MongoDB:', error.message);
